98a4b0b87a66df064ffb57b0ca85ac40a81f7f47
Vladimir Vasilev <Vlado_88g@abv.bg>
2021-03-22 16:19:28 +0200

    Some more css
    Some more css

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
 return(
     <header id='site-header'>
          <ul>
+             <li className='eDiary-text'>Електронен<br/> дневник </li>
             <li className='navButtons'><a href="#">8 клас</a></li>
             <li className='navButtons'><a href="#">9 клас</a></li>
             <li className='navButtons'><a href="#">10 клас</a></li>
